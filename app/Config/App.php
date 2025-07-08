fe34dccb059ba9da56f1e9be9d50359d820b6953
USER <altaf.techexactly@gmail.com>
2025-07-08 12:30:54 +0530

    Update environment and app configuration settings

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/asset-management-sheet/';
+    public string $baseURL = 'https://mole-maximum-fawn.ngrok-free.app/asset-management-sheet/public/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
